381617820d48e62229751100af46a2c1387eb1b6
ianhancock1970 <ian@miromedia.co.uk>
2025-09-21 11:46:00 +0100

    Add migration for 20250921_104510 to index.ts

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,5 +1,6 @@
 import * as migration_20250909_161006_initial from './20250909_161006_initial';
 import * as migration_20250920_135157_add_nested_docs_to_pages from './20250920_135157_add_nested_docs_to_pages';
+import * as migration_20250921_104510 from './20250921_104510';
 
 export const migrations = [
   {
@@ -10,6 +11,11 @@ export const migrations = [
   {
     up: migration_20250920_135157_add_nested_docs_to_pages.up,
     down: migration_20250920_135157_add_nested_docs_to_pages.down,
-    name: '20250920_135157_add_nested_docs_to_pages'
+    name: '20250920_135157_add_nested_docs_to_pages',
+  },
+  {
+    up: migration_20250921_104510.up,
+    down: migration_20250921_104510.down,
+    name: '20250921_104510'
   },
 ];
