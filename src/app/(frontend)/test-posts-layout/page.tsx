04869351913365d40f219cd622cb46323d532f52
ianhancock1970 <ian@miromedia.co.uk>
2025-10-02 15:08:36 +0100

    Cleaned up test files

--- src/app/(frontend)/test-posts-layout/page.tsx
+++
@@ -1,9 +0,0 @@
-import { BlogHeaderFeaturedPost01 } from '@/components/uui/marketing/blog/blog-header-featured-post-01'
-
-export default function TestPostLayout() {
-  return (
-    <div>
-      <BlogHeaderFeaturedPost01 />
-    </div>
-  )
-}
